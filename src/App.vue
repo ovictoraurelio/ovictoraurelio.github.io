a4e6c09c2f6727f2f2bfc392540b6448bafa376d
Victor Aurélio <ovictoraurelio@gmail.com>
2020-05-21 22:08:24 -0300

    initial project structure

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  v-app
+    v-app-bar(app color='primary' dark)
+      .d-flex.align-center        
+      v-spacer
+    v-content
+      Home
+</template>
+
+<script>
+import Home from './views/Home';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
