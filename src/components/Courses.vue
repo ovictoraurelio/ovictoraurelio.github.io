24669a9d1f7660dba13bce831cbca690c8da3a02
Victor Aurélio <ovictoraurelio@gmail.com>
2021-10-07 14:46:25 -0300

    feat: flat design and timeline

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,16 +1,23 @@
 <template lang="pug">
-  v-card.pa-8(outlined)
+  v-card.pa-0(flat)
     v-card-title.display-2.font-weight-thin skills and expertises
-    v-card-text.mt-8
+    v-card-text.mt-8.pa-0
       v-row.no-gutters(align="start" justify="start" v-for="(group, index) in groups")
         v-col.mt-8.no-gutters(cols=12)
           v-row.no-gutters.title.font-weight-thin {{group[0].type}}s
-        v-col.no-gutters(cols=12)
-          v-row.mt-0
-            v-col(v-for="(course, index) of group" :key="index" cols="auto")
-              v-card(max-width='344' outlined)
-                v-card-title.headline {{course.text}}
-                v-card-subtitle.overline {{course.type}}
+        v-col.no-gutters.pa-0(cols=12)
+          v-row.mt-0(justify="center")
+            v-col(v-for="(course, index) of group" :key="index" cols="auto" align="center")
+              v-card.text-center(width='400' height='250' outlined)
+                v-row.mt-8(no-gutters)
+                  v-col
+                    iconify(style="font-size: 50px;" :icon='course.icon')
+                v-row(no-gutters)
+                  v-col
+                    .headline {{course.text}}
+                v-row(no-gutters)
+                  v-col
+                    .overline {{course.type}}
 </template>
 
 <script>
@@ -19,39 +26,39 @@ export default {
   data: () => ({
     groups: [],
     courses: [
-      { type: 'Cloud Service', text: "Google Cloud"},
-      { type: 'Cloud Service', text: "Container"},
-      { type: 'Cloud Service', text: "Load Balancing"},
-      { type: 'Cloud Service', text: "Kubernetes"},
-      { type: 'Cloud Service', text: "Compute Engine & EC2"},
-      { type: 'Cloud Service', text: "Cloud SQL & RDS"},
-      { type: 'Cloud Service', text: "Cloud Functions & Lambda"},
-      { type: 'Cloud Service', text: "Cloud Run"},
-      { type: 'Cloud Service', text: "Firebase Hosting"},
-      { type: 'Cloud Service', text: "Firebase Firestore"},
-      { type: 'Cloud Service', text: "Firebase Extensions"},
-      { type: 'Cloud Service', text: "Network & Firewall"},
-      { type: 'Language', text: "Linguagem C" },
-      { type: 'Language',  text: "Java" },
-      { type: 'Language', text: "Kotlin Android" },      
-      { type: 'Language', text: "C++" },
-      { type: 'Language', text: "Assembly Mips" },
-      { type: 'Language', text: "Assembly x86" },
-      { type: 'Language', text: "Javascript" },
-      { type: 'Language', text: "WebAssembly" },      
-      { type: 'Language', text: "Php" },
-      { type: 'Language', text: "Sass" },
-      { type: 'Framework', text: "Laravel" },
-      { type: 'Framework', text: "Express.JS" },
-      { type: 'Framework', text: "Sequelize" },
-      { type: 'Framework', text: "Firebase" },
-      { type: 'Framework', text: "Docker" },
-      { type: 'Database', text: "My SQL" },
-      { type: 'Database', text: "PostgreSQL" },
-      { type: 'Database', text: "MariaDB" },
-      { type: 'Database', text: "Firestore" },
-      { type: 'Database', text: "Mongo DB" },
-      { type: 'Database', text: "SQL Lite" }
+      { icon: 'logos-google-cloud', type: 'Cloud Service', text: "Google Cloud"},
+      { icon: 'vscode-icons:file-type-devcontainer', type: 'Cloud Service', text: "Container"},
+      { icon: 'carbon:load-balancer-local', type: 'Cloud Service', text: "Load Balancing"},
+      { icon: 'logos-kubernetes', type: 'Cloud Service', text: "Kubernetes"},
+      { icon: 'logos-google-cloud', type: 'Cloud Service', text: "Compute Engine & EC2"},
+      { icon: 'logos-google-cloud', type: 'Cloud Service', text: "Cloud SQL & RDS"},
+      { icon: 'logos-google-cloud-functions', type: 'Cloud Service', text: "Cloud Functions & Lambda"},
+      { icon: 'logos-google-cloud-run', type: 'Cloud Service', text: "Cloud Run"},
+      { icon: 'logos-firebase', type: 'Cloud Service', text: "Firebase Hosting"},
+      { icon: 'logos-firebase', type: 'Cloud Service', text: "Firebase Firestore"},
+      { icon: 'logos-firebase', type: 'Cloud Service', text: "Firebase Extensions"},
+      { icon: 'mdi-network', type: 'Cloud Service', text: "Network & Firewall"},
+      { icon: 'logos-c', type: 'Language', text: "Linguagem C" },
+      { icon: 'logos-java', type: 'Language',  text: "Java" },
+      { icon: 'logos-kotlin', type: 'Language', text: "Kotlin Android" },      
+      { icon: 'logos-c-plusplus', type: 'Language', text: "C++" },
+      { icon: 'file-icons:assembly-generic', type: 'Language', text: "Assembly Mips" },
+      { icon: 'file-icons:assembly-generic', type: 'Language', text: "Assembly x86" },
+      { icon: 'logos-javascript', type: 'Language', text: "Javascript" },
+      { icon: 'logos-webassembly', type: 'Language', text: "WebAssembly" },      
+      { icon: 'logos-php', type: 'Language', text: "Php" },
+      { icon: 'logos-sass', type: 'Language', text: "Sass" },
+      { icon: 'logos-laravel', type: 'Framework', text: "Laravel" },
+      { icon: 'logos-nodejs', type: 'Framework', text: "Express.JS" },
+      { icon: 'logos-sequelize', type: 'Framework', text: "Sequelize" },
+      { icon: 'logos-firebase', type: 'Framework', text: "Firebase" },
+      { icon: 'logos-docker', type: 'Framework', text: "Docker" },
+      { icon: 'logos-mysql', type: 'Database', text: "My SQL" },
+      { icon: 'logos-postgresql', type: 'Database', text: "PostgreSQL" },
+      { icon: 'logos-mariadb', type: 'Database', text: "MariaDB" },
+      { icon: 'logos-firebase', type: 'Database', text: "Firestore" },
+      { icon: 'logos-mongodb', type: 'Database', text: "Mongo DB" },
+      { icon: 'vscode-icons:file-type-sql', type: 'Database', text: "SQL Lite" }
       
     ]
   }),
