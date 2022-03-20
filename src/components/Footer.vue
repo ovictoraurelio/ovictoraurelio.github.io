d1ef13f9e1d2f21d8d67f2c29b7fb6dc5743c8e5
Victor Aurélio <ovictoraurelio@gmail.com>
2022-03-20 16:35:13 -0300

    refactor: migrating from vuetify to windicss

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
-v-footer(padless)
-  v-container.mt-8.ma-0.pa-0(fluid)
-    v-card.ma-0.pa-0.blue-grey.darken-4.white--text.text-center(flat tile)
-      v-card-text
-        v-btn.mx-4.white--text.elevation-1(v-for='media in medias' :key='media.link' icon @click="openUrl(media)")
-          v-icon(size='24px') {{ media.icon }}
-      v-card-text.white--text.pt-0
-        | Site currículo minimalista desenvolvido no tempo livre.
-      v-divider
-      v-card-text.white--text
-        | {{ new Date().getFullYear() }} &mdash; 
-        strong Victor Aurélio
+footer(padless)
+  .divide-y.divide-gray-300.my-4
+    .flex-grow
+    .flex-grow
+  div
+    button.border-gray-500.text-gray.font-bold.p-2.rounded-full.mx-4(v-for='media in medias' :key='media.link' icon @click="openUrl(media)")
+      iconify(style="font-size: 24px" :icon="media.icon")
+  //- .font-light.pt-4
+    | Site currículo minimalista desenvolvido no tempo livre.
+  div.mb-12
+    | {{ new Date().getFullYear() }} &mdash; 
+    strong Victor Aurélio
 </template>
 
 <script>
