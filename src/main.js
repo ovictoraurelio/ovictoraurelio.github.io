823c9bea6ae088d2b7a49c58d6b4c4f8034522ea
Victor Aurélio <ovictoraurelio@gmail.com>
2022-03-20 16:34:22 -0300

    feat: remove unused robotfont because webfontloader isn't used anymore

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 import { ViteSSG } from '@vuetify/vite-ssg/single-page'
 import App from './App.vue'
 import 'virtual:windi.css'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
 
 import { Icon } from '@iconify/vue';
 
-export const createApp = ViteSSG(App, ({ app,  }) => {
-  
+export const createApp = ViteSSG(App, ({ app,  }) => {  
   app
     .component('iconify', Icon)
-  
-  app.config.globalProperties.$isMobile = () => {
-    return false
-  }
-  
+    .config.globalProperties.$isMobile = () => {
+      return false
+    }
 })
 
