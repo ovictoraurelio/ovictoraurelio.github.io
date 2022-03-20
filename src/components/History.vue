30cdfc342bcb8df09ea0609c9ca2ec78d2baadaa
Victor Aurélio <ovictoraurelio@gmail.com>
2022-03-20 11:49:02 -0300

    feat: a better mobile columns layout to services section

--- src/components/History.vue
+++ src/components/History.vue
@@ -6,9 +6,9 @@ v-container
   v-row(justify="start" align="center" v-for="(history, index) of histories" :key="index")
     v-col(cols=1)
       v-icon.h6(color="grey") mdi-circle-small
-    v-col(cols=2)
+    v-col(cols=3 md=2)
       .text-caption {{ history.date }}
-    v-col(cols='auto' style="text-align: start")
+    v-col(cols=8 md=9 style="text-align: start")
       .font-weight-bold {{ history.locate.name }}
       .text-caption {{ history.office }}
 </template>
