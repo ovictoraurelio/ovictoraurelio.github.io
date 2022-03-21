87222f1de3d28b5ded8dd15d9f3d06e15d9bb07e
Victor Aurélio <ovictoraurelio@gmail.com>
2022-03-21 15:13:38 -0300

    chore: add i18n package to the project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
 
 import path from 'path'
 
@@ -10,7 +11,10 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    WindiCSS()
+    WindiCSS(),
+    vueI18n({
+      include: path.resolve(__dirname, 'src/locales/**')
+    })
   ],
   define: { 'process.env': {} },
   resolve: {
