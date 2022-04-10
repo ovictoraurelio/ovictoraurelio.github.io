b6bf8ddeb5bb890efcf806811683a84e2215eb0e
Victor Aurélio <ovictoraurelio@gmail.com>
2022-04-10 13:04:57 -0300

    chore: add eslint and prettier with js standard style

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
-# Victor Aurélio 
+# Victor Aurélio
 
 ## Project setup
+
 ```
-yarn 
+npm i
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
-yarn serve
+npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
-yarn build
+npm run build
 ```
 
 ### Lints and fixes files
+
 ```
-yarn lint
+npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import eslintPlugin from 'vite-plugin-eslint'
 
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-  },
+  build: {},
   plugins: [
+    eslintPlugin(),
     vue(),
     WindiCSS(),
     vueI18n({
@@ -19,9 +20,9 @@ export default defineConfig({
   define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
