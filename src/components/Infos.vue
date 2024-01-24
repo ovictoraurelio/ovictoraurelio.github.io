94e7260ccebeb9d84714063219585314d6985607
Victor Aurelio <ovictoraurelio@gmail.com>
2024-01-24 16:30:27 -0300

    refactor: Infos.vue component to use ImageRounded component and add hideDescription prop

--- src/components/Infos.vue
+++ src/components/Infos.vue
@@ -1,12 +1,11 @@
 <template lang="pug">
 .container
+  .grid.grid-cols-12.grid-rows-1(v-if="$isMobile()")
+    .col-start-4.col-span-6.text-center.content-center.justify-center.justify-items-center
+      ImageRounded.justify-self-center(v-if="$isMobile()")
   .flex
-    .flex-col
-      img.rounded-full(
-        :style="imgStyle"
-        src='https://firebasestorage.googleapis.com/v0/b/ovictoraurelio.appspot.com/o/Pi7compressedIMG_6435-small-minified.webp?alt=media&token=582014d5-5a59-47ae-8e85-b2e037fb1e74'
-        alt='Victor Aurélio'
-      )
+    .w-50(v-if="!$isMobile()")
+      ImageRounded
     .basis-full.flex-col.ml-2
       .flex
         .flex-col
@@ -15,27 +14,37 @@
           button(size="sm" class="my-2 my-sm-0 btn-info" type="submit" @click="alternateLanguage")
             iconify(icon="cil:language")
       .flex
-        .flex-col
-          span.text-xl.pt-0.pb-0.font-medium {{ $t("infos.title") }}
+        .flex-col.text-left
+          span.pt-0.pb-0.font-medium {{ $t("infos.title") }}
       .flex
         .flex-col
           span.text-sm.py-0 {{ $t("infos.subtitle") }}
       .flex
         .flex-col
           span.text-sm.pt-2 {{ $t("infos.location") }}
-  .flex.mt-2
+  .flex.mt-2(v-if="!hideDescription")
     .flex-col
       .text-base.text-justify(v-html='$t("infos.description")')
 </template>
 
 <script>
+import ImageRounded from './ImageRounded.vue'
 export default {
   name: 'Home',
-  computed: {
-    imgStyle() {
-      const length = this.$isMobile() ? 80 : 120
-      return `width: ${length}px;height: ${length}px;`
+  components: {
+    ImageRounded
+  },
+  props: {
+    hideDescription: {
+      type: Boolean,
+      default: false
     }
+  },
+  computed: {
+    // imgStyle() {
+    //   const length = this.$isMobile() ? 80 : 120
+    //   return `width: ${length}px;height: ${length}px;`
+    // }
   },
   methods: {
     alternateLanguage() {
@@ -49,10 +58,5 @@ export default {
 </script>
 
 <style scoped>
-img {
-  display: block;
-  width: 100vw;
-  height: 100vh;
-  object-fit: cover; /* or object-fit: contain; */
-}
+
 </style>
