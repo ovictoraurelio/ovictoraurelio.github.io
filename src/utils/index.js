13bf717a22e573b00166e2df7a3d1a64dc495901
Victor Aurelio <ovictoraurelio@gmail.com>
2024-01-24 16:43:12 -0300

    refactor: isMobile function to handle window object check

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
-export function isMobile () {
-  return /Android|webOS|iPhone/i.test(
-    navigator.userAgent
-  )
+export function isMobile() {
+  if (typeof window === 'undefined') {
+    return false
+  }
+  
+  return /Android|webOS|iPhone/i.test(navigator.userAgent)
 }
