b9928510cab17b689b064d3dbd5b886d27208660
Victor Aurélio <ovictoraurelio@gmail.com>
2024-11-16 10:09:19 -0300

    fix: update media button click handler in Footer.vue to use media.link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@ footer(padless)
     .flex-grow
     .flex-grow
   div
-    button.border-gray-500.text-gray.font-bold.p-2.rounded-full.mx-4(v-for='media in medias' :key='media.link' icon @click="$openUrl(media)")
+    button.border-gray-500.text-gray.font-bold.p-2.rounded-full.mx-4(v-for='media in medias' :key='media.link' icon @click="$openUrl(media.link)")
       iconify(style="font-size: 24px" :icon="media.icon")
   //- .font-light.pt-4
     | Site currículo minimalista desenvolvido no tempo livre.
